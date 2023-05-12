f03bece5fcdb941b76fe10c7cf6841f313d49cfa
AGaitanBmx <agustin.gaitan4130@gmail.com>
2023-05-11 23:33:23 -0300

    backend add

---
+++ ProyectoIntegradorAP_AgustinAndrade/BackEnd/src/main/java/com/porfolio/agustinandrade/AgustinandradeApplication.java
@@ -0,0 +1,13 @@
+package com.porfolio.agustinandrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgustinandradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgustinandradeApplication.class, args);
+	}
+
+}
